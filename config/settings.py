0f7d195c2317b9c8ddea52780ee40aefe7bf664e
kotaSHIOZAKI <ksw2070042@stu.o-hara.ac.jp>
2021-09-17 17:17:15 +0900

    インデックス更新

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'shiozaki.apps.ShiozakiConfig',
-    'hayatapp.apps.HayatappConfig',
 ]
 
 MIDDLEWARE = [
