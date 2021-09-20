b86cb7df9867e571afb50aaaf5c414e8902a6bb5
yuzua <k.saito70329@gmail.com>
2021-09-20 20:04:06 +0900

    [fix] I forgot to push

--- bba/urls.py
+++ bba/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'bba'
 urlpatterns = [
     path('',views.IndexView.as_view(), name='index'),
+    # path('bba-list/', views.BbaListView.as_view(), name='bba_list'),
+    # path('bba-detail/<int:pk>', views.BbaListView.as_view(), name='bba_detail'),
+    # path('bba-create/', views.BbaListView.as_view(), name='bba_create'),
 ]
