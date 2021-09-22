65f3541fd6cd88126d5b56825c2abc3080405a52
yuzua <k.saito70329@gmail.com>
2021-09-22 11:03:13 +0900

    [update] スレッドの表示完成

--- bba/views.py
+++ bba/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
-from .models import Thread
+from .models import Comment, Thread
 from .forms import ThreadForm
 
 # Create your views here.
@@ -9,17 +9,21 @@ class IndexView(generic.TemplateView):
     template_name = "bba/base.html"
 
 class ThreadListView(generic.ListView):
-    model = Thread 
+    model = Thread
     template_name = 'bba/thread.html'
 
     def get_queryset(self):
-        thread_list = Thread.objects.all().order_by('-created_at') 
+        thread_list = Thread.objects.all().order_by('created_at') 
         return thread_list
 
-class ThreadCreateView(generic.CreateView):
-    from_class = ThreadForm
-    temaplate_name = 'bba/thread.html'
-    success_url = reverse_lazy('bba/thread.html')
+# class ThreadCreateView(generic.CreateView):
+#     from_class = ThreadForm
+#     temaplate_name = 'bba/thread.html'
+#     success_url = reverse_lazy('bba/thread.html')
     
-class CommentView(generic.ListView):
-    pass
+# class CommentListView(generic.ListView):
+#     model = Comment
+#     template_name = 'bba/comment.html'
+
+# class CommentCreateView(generic.CreateView):
+#     pass
