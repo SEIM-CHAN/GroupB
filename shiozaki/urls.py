840bbf6dc78a21e3f88061e1edf9a9fe489dd4e5
kotaSHIOZAKI <ksw2070042@stu.o-hara.ac.jp>
2021-09-26 12:56:43 +0900

    掲示板作成、更新の実装

--- shiozaki/urls.py
+++ shiozaki/urls.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 #config/urls.pyにて、path('', include('shiozaki.urls')),を追加
 urlpatterns = [
     path('', views.ShiozakiView.as_view(), name='index'),
-    path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
     path('board-list/', views.BoardListView.as_view(), name='board-list'),
     path('board-detail/', views.BoardDetailView.as_view(), name='board-detail'),
+    path('board-create/', views.BoardCreateView.as_view(), name='board-create'),
+    path('board-update/', views.BoardUpdateView.as_view(), name='board-update'),
 ]
