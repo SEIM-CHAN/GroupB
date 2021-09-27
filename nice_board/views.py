e0aeb1eed59d9a991aa35317d2f743a182397540
tamaki1121 <koizumi88ohara@gmail.com>
2021-09-28 08:26:17 +0900

    [update] 追加 編集や削除時にユーザーの検証を行う機能

--- nice_board/views.py
+++ nice_board/views.py
@@ -1,15 +1,41 @@
 from django.http import request
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.contrib import messages
 from django.urls import reverse_lazy, reverse
+from django.shortcuts import redirect
+
 from .models import NiceThread, NiceComment
 from .forms import NiceThreadCreateForm, NiceCommentCreateForm, NiceCommentUpdateForm
 
+"""tools
+"""
+class Verification():
+    """検証用クラス"""
+    def verificationThreadUser(self, id = None):
+        """リクエストユーザーとスレッドユーザーの検証"""
+        if id is None:
+            id = self.kwargs['pk']
+        target = NiceThread.objects.filter(id=id).first()
+        return (not target is None) and (target.user.id == self.request.user.id)
+    
+    def verificationCommentUser(self, id = None):
+        """リクエストユーザーとコメントユーザーの検証"""
+        if id is None:
+            id = self.kwargs['pk']
+        target = NiceComment.objects.filter(id=id).first()
+        return (not target is None) and ((not target.user is None) and (target.user.id == self.request.user.id))
+
+
+"""Common Views
+"""
 class IndexView(generic.TemplateView):
     """トップ"""
     template_name = 'nice_board/index.html'
 
+"""Thread View
+"""
 class NiceThreadListView(generic.ListView):
     """スレッドリスト"""
     template_name = 'nice_board/threads/list.html'
@@ -43,30 +69,54 @@ class NiceThreadCreateView(LoginRequiredMixin,generic.CreateView):
         messages.success(self.request, 'スレッドの作成に失敗しました')
         return super().form_invalid(form)
 
-class NiceThreadUpdateView(LoginRequiredMixin, generic.UpdateView):
+
+class NiceThreadUpdateView(LoginRequiredMixin, generic.UpdateView, Verification):
     """スレッド編集"""
     template_name = 'nice_board/threads/update.html'
     model = NiceThread
     form_class = NiceThreadCreateForm
+
+    def get(self, request, *args, **kwargs):
+        if super().verificationThreadUser():
+            return super().get(request, *args, **kwargs)
+        else:
+            return redirect('nice_board:index')
+
     def form_valid(self, form):
-        messages.success(self.request, 'スレッドを変更しました')
-        return super().form_valid(form)
+        if super().verificationThreadUser():
+            messages.success(self.request, 'スレッドを変更しました')
+            return super().form_valid(form)
+        else:
+            return self.form_invalid(form)
+            
     def form_invalid(self, form):
-        messages.success(self.request, 'スレッドの変更に失敗しました')
+        messages.error(self.request, 'スレッドの変更に失敗しました')
         return super().form_invalid(form)
+
     def get_success_url(self):
         return reverse_lazy('nice_board:threads')
 
-class NiceThreadDeleteView(LoginRequiredMixin, generic.DeleteView):
+class NiceThreadDeleteView(LoginRequiredMixin, generic.DeleteView, Verification):
     """スレッド削除"""
     template_name = 'nice_board/threads/delete.html'
     model = NiceThread
     success_url = reverse_lazy('nice_board:threads')
+
+    def get(self, request, *args, **kwargs):
+        if super().verificationThreadUser():
+            return super().get(request, *args, **kwargs)
+        else:
+            return redirect('nice_board:index')
+
     def delete(self, *args, **kwargs):
-        messages.success(self.request, "スレッドを削除しました")
-        return super().delete(request, *args, **kwargs)
+        if super().verificationThreadUser():
+            messages.success(self.request, "スレッドを削除しました")
+            return super().delete(request, *args, **kwargs)
+        else:
+            return redirect('nice_board:index')
 
-
+"""Comment View
+"""
 class NiceCommentListView(generic.ListView):
     """コメント一覧"""
     template_name = 'nice_board/comments/list.html'
@@ -124,12 +174,19 @@ class NiceCommentCreateToolView(NiceCommentCreateView):
         return reverse('nice_board:comment_create_tool', kwargs={'pk': self.kwargs['pk']})
 
 
-class NiceCommentUpdateView(LoginRequiredMixin, generic.UpdateView):
+class NiceCommentUpdateView(LoginRequiredMixin, generic.UpdateView, Verification):
     """コメント削除"""
     template_name = 'nice_board/comments/update.html'
     model = NiceComment
     form_class = NiceCommentCreateForm
     pk_url_kwarg = 'pk2'
+
+    def get(self, request, *args, **kwargs):
+        if super().verificationThreadUser(self.kwargs['pk']) or super().verificationCommentUser(self.kwargs['pk2']):
+            return super().get(request, *args, **kwargs)
+        else:
+            return redirect('nice_board:index')
+
     def get_context_data(self, **kwargs):
         """スレッド番号添付"""
         context = super().get_context_data(**kwargs)
@@ -137,15 +194,26 @@ class NiceCommentUpdateView(LoginRequiredMixin, generic.UpdateView):
             'pk': self.kwargs['pk'],
             })
         return context
-    def get_success_url(self):
-        return reverse_lazy('nice_board:threads')
+    
     def form_valid(self, form):
-        comment = form.save(commit=False)
-        comment.user = None
-        comment.text = "投稿は削除されました"
-        comment.save()
-        messages.success(self.request, 'コメントを削除しました')
-        return super().form_valid(form)
+        pageAdmin = super().verificationThreadUser(self.kwargs['pk'])
+        sameUser = super().verificationCommentUser(self.kwargs['pk2'])
+        if pageAdmin or sameUser:
+            comment = form.save(commit=False)
+            comment.user = None
+            if pageAdmin and (not sameUser):
+                text = "コメントは管理者によって削除されました"
+            else:
+                text = "コメントは削除されました"
+            comment.text = text
+            comment.save()
+            messages.success(self.request, 'コメントを削除しました')
+            return super().form_valid(form)
+        else:
+            return redirect('nice_board:index')
     def form_invalid(self, form):
         messages.success(self.request, 'コメントの削除に失敗しました')
         return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('nice_board:threads')
