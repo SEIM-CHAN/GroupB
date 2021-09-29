1162ed89715eb4b0b8f1ba671b6926d6c068e9ff
KotaSHIOZAKI <ksw2070042@stu.o-hara.ac.jp>
2021-09-29 10:45:22 +0900

    settings.pyをEC2対応に変更

--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'MyDB',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'adminpass',
+    #     'HOST': 'mysqltest.clh3pmnwcse0.us-east-1.rds.amazonaws.com',
+    #     'PORT': '3306',
+    # }
 }
 
 
